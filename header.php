65b9a22347bc5bc7d429f0d19481bc0097f493d7
anne-sophie-balestra <anne.sophie.balestra@gmail.com>
2015-01-29 16:31:54 +0100

    First commit

---
+++ header.php
@@ -0,0 +1,47 @@
+<?php
+/********************************************
+* header.php                                *
+* Ouverture de Html + menu                  *
+*                                           *
+* Auteurs : Anne-Sophie Balestra            *
+*           Abdoul Wahab Haidara            *
+*           Yvan-Christian Maso             *
+*           Baptiste Quere                  *
+*           Yoann Le Taillanter             *
+*                                           *
+* Date de creation : 29/01/2015             *
+* Date de dernière modif : 29/01/2015       *
+********************************************/
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Solent 2 - Facturier</title>
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="librairies/bootstrap-3.3.2-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="librairies/bootstrap-3.3.2-dist/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" type="text/css" href="librairies/font-awesome-4.3.0/css/font-awesome.min.css">
+    </head>
+
+    <body>
+        <nav class="navbar navbar-inverse" role="navigation">
+            <div class="container">
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="text-center"><a href="index.php"><i class="icon-home fa fa-home fa-2x icon-2x"></i><br> Accueil</a></li>
+                        <li class="dropdown text-center">
+                            <a href="#menuUbat" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="glyphicon-th-list fa fa-th-list fa-2x icon-2x"></i><br>Gestion UBAT<b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu text-left">
+                                <li class="dropdown-header">Ajouts</li>
+                                <li><a href="index.php?action=ajoutConstructeur"><i class="icon-plus fa fa-plus"></i> Nouveau constructeur...</a></li>
+                                <li><a href="index.php?action=ajoutChantier"><i class="icon-plus fa fa-plus"></i> Nouveau chantier...</a></li>
+                                <li><a href="index.php?action=ajoutDocument"><i class="icon-plus fa fa-plus"></i> Nouveau document bibliothèque...</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
