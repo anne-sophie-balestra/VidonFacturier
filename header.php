d4404b0b1779b3a9a18f9bc1fd2763b2c4970c58
yletaillanter <y.letaillanter@gmail.com>
2015-03-16 14:56:17 +0100

    Ajout du menu dans header

--- header.php
+++ header.php
@@ -17,6 +17,34 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Solent 2 - Facturier</title>
+        <script src="librairies/dhtmlxSuite/codebase/dhtmlx.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="librairies/dhtmlxSuite/codebase/dhtmlx.css">
+
+        <script>
+            var myMenu;
+            function doInitMenu() {
+            	//Creer un nouveau menu
+                myMenu = new dhtmlXMenuObject("parentId");
+
+                // Inflate la structure du menu dans le fichier data/menu.xml
+                myMenu.loadStruct("data/menu.xml", function(){
+                    
+                    //Gestion des evénements lorsqu'on clique sur les éléments du menu.
+                    myMenu.attachEvent("onClick", function(id, zoneId, cas){
+                        if ( id == "fTop") {
+                            if(id == "fac_man") {
+                                alert("facture manuelle!");                        
+                            }
+                        } else {
+                            alert("test");
+                        }
+                    });
+
+                });
+            }
+        </script>  
+
     </head>
 
-    <body>
+    <body onload="doInitMenu();"> 
+        <div id="parentId"></div>
