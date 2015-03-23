68ae1d9c27a72957dbd89048b0c2f7dbc8b5b871
Baptiste-Q <baptiste.quere.1@etudiant.univ-rennes1.fr>
2015-03-23 16:20:12 +0100

    Ajout commentaires

--- pages/facture/administrateurs/createModel.php
+++ pages/facture/administrateurs/createModel.php
@@ -13,6 +13,7 @@
  ********************************************/
 $pdo = new SPDO();
 
+//On recupere les types de dossiers existants
 $stmt_type_dossier = "SELECT DISTINCT(t_dos_type) FROM type_dossier ORDER BY t_dos_type";
 $result_type_dossier = $pdo->prepare ( $stmt_type_dossier );
 $result_type_dossier->execute();
@@ -20,20 +21,25 @@ $result_type_dossier->execute();
 ?>
 <!-- Contenu principal de la page -->
 <div class="container">
-	<form id="formNewModel" action="index.php?action=insertModel" method="post" role="form" data-toggle="validator">
+
+    <!--Creation d'un formulaire avec la validation Bootstrap-->
+    <form id="formNewModel" action="index.php?action=insertModel" method="post" role="form" data-toggle="validator">
 		<h2>Nouveau Modèle</h2>
+        <!--Renseignement du nom du modele-->
 		<div class="form-group">
 			<label class="control-label" for="name">Nom du modèle :</label>
 			<input name="name" type="text" required class="form-control" id="name" maxlength="255" data-error="Veuillez entrer le nom du modèle">
 			<div class="help-block with-errors"></div>
 		</div>
-		
+
+        <!--Renseignement de la zone geographique-->
 		<div class="form-group">
 			<label class="control-label" for="operation">Zone géographique </label>
 			<input type="radio" name="area" value="France" checked> France <input
 				type="radio" name="area" value="Etranger"> Etranger
 		</div>
 
+        <!--On demande a l'utilisateur quel est le type de dossier concerne par le modele de facture-->
 		<div class="form-group">
 			<label class="control-label" for="operation">Type de dossier :</label>
 			<select name="type_dossier" id="type_dossier" required class="form-control select2">
@@ -42,28 +48,31 @@ $result_type_dossier->execute();
             foreach($result_type_dossier->fetchAll(PDO::FETCH_OBJ) as $type_dossier) { ?>
                  <option value="<?php echo $type_dossier->t_dos_type; ?>"><?php echo $type_dossier->t_dos_type; ?></option>
             <?php } ?>
-                    
             </select>
 		</div>
-		
+
+        <!--Renseignement de l'objet de la facture-->
 		<div class="form-group">
 			<label class="control-label" for="objet">Objet de la facture :</label>
 			<input name="objet" type="text" required class="form-control" id="objet" maxlength="255" data-error="Veuillez entrer l'objet de la facture">
 			<div class="help-block with-errors"></div>
 		</div>
 
+        <!--Renseignement de la langue de la facture-->
 		<div class="form-group">
 			<label class="control-label" for="language">Langue de la facture :</label>
 			<input name="language" type="text" value="Français" required class="form-control" id="language" maxlength="255" data-error="Veuillez entrer la langue de la facture">
 			<div class="help-block with-errors"></div>
 		</div>
-		
+
+        <!--Renseignement du taux de TVA-->
 		<div class="form-group">
 			<label class="control-label" for="TVA">TVA :</label> 
 			<input name="TVA" type="number" value="20" required class="form-control" id="number" maxlength="255" data-error="Veuillez entrer le taux de TVA">
 			<div class="help-block with-errors"></div>
 		</div>
-			
+
+        <!--Validation du formulaire-->
 		<div>
 			<input type="submit" name="button" class="btn btn-success" id="button" value="Ajouter"> <a href="#" onclick="history.back()" class="btn btn-danger" title="Annuler">Annuler</a>
 		</div>
@@ -74,20 +83,8 @@ $result_type_dossier->execute();
 
 <script type="text/javascript" charset="utf-8">
     $(document).ready(function() {
-        $("#operation").select2({
-            placeholder: "Choisissez une opÃ©ration..."
-        });
-        $("#ent_dossier").select2({
-            placeholder: "Choisissez une entitÃ©..."
-        });
         $("#type_dossier").select2({
-            placeholder: "Choisissez un type..."
-        });
-        $("#nom_code").select2({
-            placeholder: "Choisissez un code..."
-        });
-        $("#pays").select2({
-            placeholder: "Choisissez un pays..."
+            placeholder: "Choisissez un type de dossier..."
         });
     });
 </script>
